81565d84fb5f860888f63e6866300134421c3cf4
Valerii Bazanov <bazanov1993@gmail.com>
2024-11-05 23:03:27 +0300

    terraform 05

---
+++ terraform-05/src/main.tf
@@ -0,0 +1,57 @@
+module "vpc_dev" {
+  source       = "./local_module_vpc"
+  vpc_name     = var.vpc_name
+  zone         = var.default_zone
+  cidr         = var.default_cidr
+}
+
+module "marketing-vm" {
+  source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
+  env_name       = "marketing" 
+  network_id     = module.vpc_dev.network_id
+  subnet_zones   = [var.default_zone]
+  subnet_ids     = [module.vpc_dev.subnet_id]
+  instance_name  = "marketing"
+  instance_count = 1
+  image_family   = var.image
+  public_ip      = var.is_public_ip
+
+  labels = { 
+    owner= "v.bazanov",
+    project = "marketing"
+  }
+
+  metadata = {
+    user-data          = data.template_file.cloudinit.rendered
+    serial-port-enable = 1
+  }
+
+}
+
+module "analystics-vm" {
+  source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
+  env_name       = "analystics"
+  network_id     = module.vpc_dev.network_id
+  subnet_zones   = [var.default_zone]
+  subnet_ids     = [module.vpc_dev.subnet_id]
+  instance_name  = "analystics"
+  instance_count = 1
+  image_family   = var.image
+  public_ip      = var.is_public_ip
+
+  labels = { 
+    owner= "v.bazanov",
+    project = "analystics"
+  }
+
+  metadata = {
+    user-data          = data.template_file.cloudinit.rendered
+    serial-port-enable = 1
+  }
+
+}
+
+data "template_file" "cloudinit" {
+  template = file("./cloud-init.yml")
+  vars = {public_key = var.public_key}
+}
---
+++ terraform-05/src/providers.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_version = "~>1.8.4"
+
+  backend "s3" {
+    
+    shared_credentials_files = ["~/.aws/credentials"]
+    shared_config_files = [ "~/.aws/config" ]
+    profile = "default"
+    region="ru-central1"
+
+    bucket     = "vbazanov-terraform-state"
+    key        = "production/terraform.tfstate"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true # Необходимая опция Terraform для версии 1.6.1 и старше.
+    skip_s3_checksum            = true # Необходимая опция при описании бэкенда для Terraform версии 1.6.3 и старше.
+
+    endpoints ={
+      dynamodb = "https://docapi.serverless.yandexcloud.net/ru-central1/b1gvdjf0m931r8sh4aca/etnfhngr1lvhlsrcr52t"
+      s3 = "https://storage.yandexcloud.net"
+    }
+
+    dynamodb_table              = "terr-lock"
+  }
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+      version = "0.118.0"
+    }
+  }
+}
+
+provider "yandex" {
+  token     = var.token
+  cloud_id  = var.cloud_id
+  folder_id = var.folder_id
+  zone      = var.default_zone
+}
